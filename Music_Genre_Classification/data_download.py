cb1d7172aadf6095e86b31c47c942c7c022b944d
Amine Gharout <aminegharout@gmail.com>
2026-02-06 20:44:56 +0100

    Third_push

---
+++ Music_Genre_Classification/data_download.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download(
+    "andradaolteanu/gtzan-dataset-music-genre-classification")
+
+print("Path to dataset files:", path)
